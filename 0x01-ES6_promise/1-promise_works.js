7be6691a60dcbb4febb213230e458106e384a060
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2024-04-18 11:00:33 +0000

    notes on promises

--- 0x01-ES6_promise/1-promise_works.js
+++ 0x01-ES6_promise/1-promise_works.js
@@ -3,7 +3,7 @@
 const packageDelivery = new Promise((resolve, reject) => {
     // simulating delivery process with set timeout
     setTimeout(() => {
-        const deliverySuccessful = Math.random() < 0.8;
+        const deliverySuccessful = Math.random() < 0.5; // 50% chance of success
         if (deliverySuccessful) {
             resolve('Package delivered');// Resolve promise if delivery successful
         }
@@ -14,3 +14,9 @@ const packageDelivery = new Promise((resolve, reject) => {
     }, 2000);// Delivery takes 2 seconds
 })
 
+// handling the promise outcome 
+packageDelivery.then((message) => {
+    console.log(message);
+}).catch((message) => {
+    console.log(message);
+});
