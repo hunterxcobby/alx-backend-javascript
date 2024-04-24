2fc69c4c698874aec039932a8e51f2785902162a
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2024-04-24 22:52:13 +0000

    Refactor ES6 arrow function for getting and adding neighborhoods in 2-arrow.js

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,12 +2,12 @@
 
 // Arrow functions are a more concise way to write a function in JavaScript.
 
-export default function getNeighborhoodsList () {
-  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square']
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  const self = this
+  const self = this;
   this.addNeighborhood = (newNeighborhood) => {
-    self.sanFranciscoNeighborhoods.push(newNeighborhood)
-    return self.sanFranciscoNeighborhoods
-  }
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
 }
