61e3cabaa51c93a468697ecd390d724b148b9226
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2024-04-25 23:51:31 +0000

    Add initializeRooms function to create and return an array of ClassRoom objects

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,13 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const classRooms = [];
+
+  const room1 = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
+
+  classRooms.push(room1, room2, room3);
+
+  return classRooms;
+}
