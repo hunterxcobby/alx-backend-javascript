8cdbce6f7e17546363d3ec50708471575d740bae
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2024-04-25 08:00:01 +0000

    Add createReportObject function in 0x00-ES6_basic/12-createReportObject.js

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+// Write a function named createReportObject whose parameter, employeesList,
+// is the return value of the previous function createEmployeesObject.
+
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
