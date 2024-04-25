3a56b03fe9d1cbb9416382c5ee4f9408343f6c0c
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2024-04-25 07:56:30 +0000

    Add new ES6 module for getFullBudgetObject function in 0x00-ES6_basic/9-getFullBudget.js

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+// Rewrite getFullBudgetObject to use ES6 method properties in the fullBudget object.
+
+/* eslint-disable */
+
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
