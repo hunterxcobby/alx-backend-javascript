064a00cb88dc530676bb93102152b034e9e9bf2f
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2024-04-27 01:06:50 +0000

    Add ES6 promise implementation in 0x01-ES6_promise/0-promise.js

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+function getResponseFromAPI() {
+  return new Promise(() => {
+    // Our API call or asynchronous operation goes here
+    // Once the operation is completed, we resolve or reject the promise accordingly
+  });
+}
+
+export default getResponseFromAPI;
