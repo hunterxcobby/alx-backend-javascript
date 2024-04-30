bec09ef9c59b868beadd970124bb651bf5c91535
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2024-04-30 18:28:47 +0000

    Refactor getListStudents function to include additional student in 0x03-ES6_data_manipulation/0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,15 @@
 export default function getListStudents() {
-  return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-  ];
+  return [{
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  }, {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  }, {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  }];
 }
