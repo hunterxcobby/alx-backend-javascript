38fbeb744cf23e02bd1d16a383d3996d00f36002
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2024-04-30 18:44:56 +0000

    Add setFromArray function in 0x03-ES6_data_manipulation/6-set.js

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,8 @@
+// This function takes an array as input and returns a new Set object
+// containing the elements of the array.
+function setFromArray(array) {
+  return new Set(array);
+}
+
+// Exporting the setFromArray function as the default export of this module.
+export default setFromArray;
