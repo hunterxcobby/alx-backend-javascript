7235a68761bcf003e0c33a303727f30db9d99b1d
hunterxcobby <solomonsefah13@gmail.com>
2024-06-10 13:12:33 +0100

    feat: Add Teacher interface to Subjects namespace

---
+++ 0x04-TypeScript/task_4/js/subjects/Teacher.ts
@@ -0,0 +1,11 @@
+// create file Teacher.ts and write Teacher interface
+// in a namespace named Subjects.
+
+// task_4/js/subjects/Teacher.ts
+
+namespace Subjects {
+  export interface Teacher {
+    firstName: string;
+    lastName: string;
+  }
+}
