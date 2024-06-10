af9f77fda2280bab9d6681207fc7d305c4bfe597
hunterxcobby <solomonsefah13@gmail.com>
2024-06-10 13:06:56 +0100

    feat: Add RowID type and RowElement interface

---
+++ 0x04-TypeScript/task_3/interface.ts
@@ -0,0 +1,12 @@
+// interface.ts
+
+// Define the RowID type
+export type RowID = number;
+
+// Define RowElement interface
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
+
