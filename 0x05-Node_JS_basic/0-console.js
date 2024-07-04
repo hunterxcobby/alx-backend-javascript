a1c90283e1d9229a95abd138ed46523bcc4f36b5
hunterxcobby <solomonsefah13@gmail.com>
2024-07-04 22:58:56 +0000

    nodejs basics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+* Function to print a message in the console
+* @param {string} message - Message to print
+*/
+
+function displayMessage(str) {
+    console.log(str); // Print the message to the console
+  }
+  
+  // Export function to make it available to other modules
+  module.exports = displayMessage;
