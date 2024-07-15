cf7df206e06ce6b830cb0b3cceac1c9a0b267715
hunterxcobby <solomonsefah13@gmail.com>
2024-07-15 14:17:39 +0000

    create a full server

---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,14 @@
+// full_server/routes/index.js
+// file to handle routing
+
+const express = require('express');
+const AppController = require('../controllers/AppController');
+const StudentsController = require('../controllers/StudentsController');
+
+const router = express.Router();
+
+router.get('/', AppController.getHomepage);
+router.get('/students', StudentsController.getAllStudents);
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
+
+module.exports = router;
