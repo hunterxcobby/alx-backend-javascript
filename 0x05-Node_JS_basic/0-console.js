43c08aa62f2180d487f070bf5dcbd299926bc18d
hunterxcobby <solomonsefah13@gmail.com>
2024-07-24 12:59:48 +0000

    Refactor displayMessage function to improve code readability

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -4,8 +4,8 @@
 */
 
 function displayMessage(str) {
-    console.log(str); // Print the message to the console
-  }
-  
-  // Export function to make it available to other modules
-  module.exports = displayMessage;
+  console.log(str); // Print the message to the console
+}
+
+// Export function to make it available to other modules
+module.exports = displayMessage;
